837b00b94e6cb02cec2c523165da5c12afb7fb28
Mario Novoselec <m.novoselec1@gmail.com>
2015-05-02 12:25:19 +0200

    Initial commit

---
+++ api/controllers/AutodeskController.js
@@ -0,0 +1,11 @@
+/**
+ * AutodeskController
+ *
+ * @description :: Server-side logic for managing Autodesks
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
